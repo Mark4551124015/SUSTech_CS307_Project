a207c7ea2cddadff67df50a7216d3e3d992649a7
Mark4551124015 <45923831+Mark4551124015@users.noreply.github.com>
2022-10-27 17:13:30 +0800

    Final Structure

--- createTables.sql
+++ createTables.sql
@@ -4,7 +4,6 @@ create table if not exists  Company
     name            varchar(50)     not null
         constraint company_pk primary key
 );
-
 create table if not exists City
 (
     city_id         SERIAL         not null,
@@ -12,15 +11,12 @@ create table if not exists City
     name            varchar(50) not null
         constraint city_pk primary key
 );
-
 create table if not exists PortCity
 (
     port_city_id    SERIAL         not null,
     name            varchar(50) not null
         constraint port_city_pk primary key
 );
-
-
 create table if not exists Courier
 (
     courier_id      SERIAL             not null ,
@@ -35,8 +31,6 @@ create table if not exists Courier
     city         varchar            not null
         constraint courier_fk_2 references city
 );
-
-
 create table if not exists Ship
 (
     ship_id         SERIAL             not null,
@@ -54,78 +48,50 @@ create table if not exists Container
         constraint Container_pk primary key,
     type            varchar(50) not null
 );
-
 create table if not exists  Delivery_Retrieval
 (
-    DR_id           SERIAL         not null
-        constraint dr_pk primary key,
-    courier      varchar         not null
-        constraint  delivery_fk references Courier,
+    DR_id           SERIAL         not null,
+    item_name       varchar(50)     not null,
     type            varchar(50)     not null,
-    date            date
-
+        courier      varchar         not null
+        constraint  delivery_fk references Courier,
+    date            date,
+        constraint dr_pk primary key(item_name,type)
 );
-
-create table if not exists  ItemType
-(
-    Item_type_id         SERIAL         not null,
-    item_type    varchar(50)         not null
-        constraint  item_type_pk primary key
-);
-
-create table if not exists  Tax
-(
-    port_city    varchar         not null
-        constraint  tax_fk_1 references portcity,
-    item_type       varchar         not null
-        constraint tax_fk_2 references ItemType,
-    export_tax      float,
-    import_tax      float,
-    last_update     date        default ('1000-01-01') not null,
-    constraint ship_detail_pk primary key (port_city, item_type)
-);
-
 create table import_export_detail
 (
-    port_id         SERIAL         not null        primary key,
+    port_id         SERIAL         not null,
+    item_name       varchar(50)     not null,
     type            varchar(50) not null,
+    item_type       varchar(50)     not null,
     port_city         varchar         not null
         constraint import_export_detail_fk references PortCity,
     tax             float         default(0)      not null,
-    date            date        not null
+    date            date        not null,
+        constraint import_export_detail_pk primary key (item_name, type)
 );
-
-create table if not exists Shipping
-(
-    shipping_id     SERIAL   not null    primary key,
-    retrieval_id    int
-        constraint shipping_fk_1 references Delivery_Retrieval,
-    export_id       int
-        constraint shipping_fk_2 references import_export_detail,
-    ship         varchar
-        constraint shipping_fk_3 references Ship,
-    container   varchar
-        constraint shipping_fk_4 references container,
-    import_id       int
-        constraint shipping_fk_5 references import_export_detail,
-    delivery_id     int
-        constraint shipping_fk_6 references Delivery_Retrieval
-);
-
 create table if not exists Shipment
 (
     shipment_id     SERIAL             not null,
     item_name       varchar(50)     not null
         constraint shipment_pk primary key,
     item_price      float             not null,
-    item_type    varchar     not null
-        constraint shipment_fk_1 references ItemType,
-    from_city    varchar             not null
+    item_type    varchar     not null,
+        from_city    varchar             not null
         constraint shipment_fk_2 references City,
     to_city      varchar             not null
         constraint shipment_fk_3 references City,
-    shipping_id     int             not null
-        constraint shipment_fk_4 references Shipping,
     log_time        timestamp      not null
 );
+create table if not exists Shipping
+(
+    shipping_id     SERIAL   not null PRIMARY KEY,
+    item_name       varchar(50)     not null
+        constraint Shipping_fk references Shipment,
+    ship         varchar
+        constraint shipping_fk_3 references Ship,
+    container   varchar
+        constraint shipping_fk_4 references container
+);
+
 
